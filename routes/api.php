d4a0350ff8e22ed2d0c391106f2ebf2ccb42685d
adityathok <aditya0thok@gmail.com>
2024-11-06 14:10:31 +0700

    users controller and route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UsersController;
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::apiResources([
+        'users' => UsersController::class,
+        // 'posts' => PostController::class,
+    ]);
 });
